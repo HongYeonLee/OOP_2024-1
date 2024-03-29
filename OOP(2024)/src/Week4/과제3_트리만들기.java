23624908f32faf0c134bd8615e1603b492a4b9ef
HongYeonLee <llhongyan@naver.com>
2024-03-29 22:27:44 +0900

    과제2 트리만들기, 랜덤, 버블정렬

--- OOP(2024)/src/Week4/과제3_트리만들기.java
+++ OOP(2024)/src/Week4/과제3_트리만들기.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class 과제3_트리만들기 {
 	
-	public static void drawTriangle1(int 높이) //왼쪽 직각 삼각형 그리기
+	public static void drawLeftTriangle(int 높이) //왼쪽 직각 삼각형 그리기
 	{
 		for (int i = 0; i <= 높이; i++) //i<=높이 주의
 		{
@@ -13,7 +13,7 @@ public class 과제3_트리만들기 {
 		}
 	}
 	
-	public static void drawTriangle2(int 높이) //오른쪽 직각 삼각형 그리기
+	public static void drawRightTriangle(int 높이) //오른쪽 직각 삼각형 그리기
 	{
 		for (int i = 0; i <= 높이; i++)
 		{
@@ -30,10 +30,6 @@ public class 과제3_트리만들기 {
 		}
 	}
 	
-	public static void drawSqure(int 가로) {
-		
-	}
-	
 	private static void drawRectangle(int 가로, int 세로) {
 		
 		//가로x세로 그리려면 for문은 세로 -> 가로 순으로 작성
@@ -46,25 +42,109 @@ public class 과제3_트리만들기 {
 		}	
 	}
 	
-	public static void main(String[] args)
+	public static void drawSqure(int 가로) 
 	{
-		
-		int 가로, 세로;
-		
-		Scanner input = new Scanner(System.in);
-		System.out.println("가로: ");
-		가로 = input.nextInt();
-		
-		System.out.println("세로: ");
-		세로 = input.nextInt();
-		
-		drawRectangle(가로, 세로);
-		drawTriangle1(세로);
-		drawTriangle2(세로);
+		for (int i = 0; i < 가로; i++)
+		{
+			for (int j = 0; j < 가로; j++)
+				System.out.print("*");
+			System.out.println();
+		}
+
 	}
 
+	public static void Tree(int 높이)
+	{
+		for (int i = 0; i < 높이; i++) {
 
+			for (int k = 0; k < 높이 - i; k++)
+				System.out.print(" ");
 
+			for (int k = 0; k < i + 1; k++) {
+				if (k % 2 == 0)
+					System.out.print("☆ ");
+				else
+					System.out.print("★ ");
+			}
+			System.out.println();
+		}
+		
+		int a = 0;
+		if (높이 % 3 == 0) a = 1;
 
+		for (int i = 0; i < 높이 / 3 ; i++) {
 
+			for (int k = 0; k < 높이-높이/4; k++)
+				System.out.print(" ");
+
+			for (int k = 0; k < 높이 / 3+1 ; k++) {
+				System.out.print("■ ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args)
+	{
+		menu();
+	}
+
+	private static void menu() {
+		
+		while(true) 
+		{
+			System.out.println("=======================================================");
+			System.out.println("menu : 1. 왼쪽 삼각형 2. 오른쪽 삼각형 3. 사각형 4. 정사각형 5. 트리 0. 그만");
+			System.out.println("=======================================================");
+			System.out.print("menu: ");
+			Scanner input = new Scanner(System.in);
+			int menu = input.nextInt();
+			
+			if (menu == 0)
+			{
+				System.out.println("종료되었습니다");
+				break;
+			}
+			
+			switch (menu)
+			{
+			case 1:
+				System.out.print("높이 : ");
+				int height = input.nextInt();
+				drawLeftTriangle(height);
+				break;
+			
+			case 2:
+				System.out.print("높이 : ");
+				height = input.nextInt();
+				drawRightTriangle(height);
+				break;
+				
+			case 3:
+				System.out.print("가로, 세로 : ");
+				int width = input.nextInt();
+				height = input.nextInt();
+				drawRectangle(width, height);
+				break;
+				
+			case 4:
+				System.out.print("높이 : ");
+				height = input.nextInt();
+				drawSqure(height);
+				break;
+			case 5:
+				System.out.println("높이 : ");
+				height = input.nextInt();
+				Tree(height);
+				break;
+				
+			default:
+				System.out.println("없는 선택지 입니다.");
+				
+			}
+		}
+
+		
+	}
 }
--- OOP(2024)/src/Week4/예제_3_12.java
+++ OOP(2024)/src/Week4/예제_3_12.java
@@ -13,41 +13,57 @@ public class 예제_3_12 {
 		System.out.println("-------------------------");
 	}
 	
-	public static void setArray(int[] ar)
+	public static void setArray(int[] arr, int start, int end, int[] checker)
 	{
-		for (int i = 0; i < ar.length; i++)
+		for (int i = 0; i < arr.length;)
 		{
-			ar[i] = (int)(Math.random()*1000)%101; //나머지 연산은 정수/정수만 가능
+			int randNum = (int)(Math.random()*1000)%(end - start + 1); //나머지 연산은 정수/정수만 가능
+			if (checker[randNum] == 1) continue;
+			checker[randNum] = 1;
+			arr[i] = randNum + start;
+			i++;
 		}
 	}
 	
-	private static void sortArray(int[] ar) {
-		for (int i = 0; i < ar.length; i++)
+	private static void sortArray(int[] arr) {
+		for (int i = 0; i < arr.length; i++)
 		{
 			//System.out.printf("%d번째 정렬 ", i);
-			for (int j = 0; j < ar.length - 1; j++)
-			if (ar[j] > ar[j+1])
+			for (int j = 0; j < arr.length - 1; j++)
+			if (arr[j] > arr[j+1])
 			{
-				int temp = ar[j];
-				ar[j] = ar[j + 1];
-				ar[j + 1] = temp;
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
 			}
 			//정렬 과정을 보고 싶으면 showArray(ar);
 		}
+		System.out.println("-------------------------");
 	}
 	
 	public static void main(String[] args)
 	{
-		int[] ar;
+		int[] arr;
 		Scanner input = new Scanner(System.in);
-		System.out.println("정수 몇개를 입력할 까요?: ");
-		int size = input.nextInt();
 		
-		ar = new int[size];
-		setArray(ar);
-		showArray(ar);
-		sortArray(ar);
-		showArray(ar);
+		System.out.print("정수 몇개를 입력할까요?: ");
+		int size = input.nextInt();
+		arr = new int[size];
+		
+		System.out.print("start: ");
+		int start = input.nextInt();
+		
+		System.out.print("end: ");
+		int end = input.nextInt();
+		
+		int[] checker = new int[end - start];
+		
+		setArray(arr, start, end, checker);
+		System.out.println("정렬 전 배열");
+		showArray(arr);
+		sortArray(arr);
+		System.out.println("정렬 후 배열");
+		showArray(arr);
 	}
 
 
