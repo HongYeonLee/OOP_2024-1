8658f2360d6cbb491c6ff2b0abc39ead7bc494ee
HongYeonLee <llhongyan@naver.com>
2024-06-01 22:01:15 +0900

    소융창 퀴즈

---
+++ 소융창/src/CourseTest.java
@@ -0,0 +1,8 @@
+
+public class CourseTest {
+	public static void main(String[] args)
+	{
+		LetterGradeCourse letterGradeCourse = new LetterGradeCourse(100, 100, 100, 100);
+		letterGradeCourse.showInfo();
+	}
+}
